d3982223d2a6b0df25d91622d3db1d311ed8c2b6
razvanfulea <razvanfulea@gmail.com>
2019-02-24 21:08:16 +0200

    Routing basics module

--- angular-routing-demo/src/app/app.module.ts
+++ angular-routing-demo/src/app/app.module.ts
@@ -3,6 +3,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeModule } from './home/home.module';
+import { PageNotFoundModule } from './page-not-found/page-not-found.module';
+import { AboutModule } from './about/about.module';
+import { HelpModule } from './help/help.module';
+import { AboutComponent } from './about/about.component';
+import { HelpComponent } from './help/help.component';
+import { ContentModule } from './content/content.module';
+import { ContentComponent } from './content/content.component';
+
+const routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: 'help', component: HelpComponent },
+  { path: 'content', component: ContentComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -10,7 +29,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([])
+    RouterModule.forRoot(routes),
+    HomeModule,
+    AboutModule,
+    HelpModule,
+    ContentModule,
+    PageNotFoundModule
   ],
   providers: [],
   bootstrap: [AppComponent]
