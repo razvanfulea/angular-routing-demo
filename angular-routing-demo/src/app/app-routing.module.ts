310bb6d40cd571b71a47704a8143c9332c4d6089
razvanfulea <razvanfulea@gmail.com>
2019-03-07 23:03:35 +0200

    Routing to features module

---
+++ angular-routing-demo/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes = [
+    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule{}
--- angular-routing-demo/src/app/content/content.module.ts
+++ angular-routing-demo/src/app/content/content.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { ContentComponent } from './content.component';
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+
+const routes = [
+    { path: 'content', component: ContentComponent }
+];
 
 @NgModule({
+    imports: [
+        SharedModule,
+        RouterModule.forChild(routes)
+    ],
     declarations: [
         ContentComponent
     ]
